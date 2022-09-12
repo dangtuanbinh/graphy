f72771e4bb5da1c873d9ec2340530bb0ba04dc7e
Robin <tuanbinh.35ggmail.com>
2022-09-12 13:40:28 +0700

    add firebase setting

---
+++ src/util/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCUZUrsLi7ORAyFHd3VSjzdF1JgkwYQsLw",
+    authDomain: "graphy-764d2.firebaseapp.com",
+    projectId: "graphy-764d2",
+    storageBucket: "graphy-764d2.appspot.com",
+    messagingSenderId: "448355242509",
+    appId: "1:448355242509:web:cdd760a26bbdb84391bb44",
+    measurementId: "G-MCX1B8SDF8"
+  };
